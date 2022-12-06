b72c65738afa18da87ddb6504b47f0c7c19e066a
PutriAuliaNur <ptrslsbl1215@gmail.com>
2022-12-06 23:10:09 +0800

    Kumpul Remedial UAS

---
+++ H071211046/Remedial UAS/Remedial UAS.sql
@@ -0,0 +1,64 @@
+#8 #AWM0101
+
+SELECT f.title AS `Movies similiar to Pulp Beverly`
+FROM category AS c
+JOIN film_category AS fc
+USING(category_id)
+JOIN film AS f
+USING(film_id)
+JOIN film_actor AS fa
+USING(film_id)
+JOIN actor AS a
+USING(actor_id)
+WHERE c.name = 'Horror' AND
+concat(a.first_name, ' ', a.last_name) = 'KIRSTEN PALTROW' | 'DUSTIN TAUTOU' | 'SUSAN DAVIS' | 'WARREN NOLTE' | 'GRETA KEITEL' | 'WHOOPI HURT' | 'ANGELA WITHERSPOON'
+GROUP BY f.title;
+
+#13 #AZV0101
+
+SELECT f.title, c.name AS genre
+FROM category AS c
+JOIN film_category AS fc
+USING(category_id)
+JOIN film AS f
+USING(film_id)
+JOIN inventory AS i
+USING(film_id)
+JOIN rental AS r
+USING(inventory_id)
+JOIN customer AS ct
+USING(customer_id)
+WHERE f.title NOT IN 
+(
+SELECT f.title
+FROM category AS c
+JOIN film_category AS fc
+USING(category_id)
+JOIN film AS f
+USING(film_id)
+JOIN inventory AS i
+USING(film_id)
+JOIN rental AS r
+USING(inventory_id)
+JOIN customer AS ct
+USING(customer_id)
+WHERE ct.customer_id = 375
+GROUP BY f.title
+)
+GROUP BY f.title;
+
+#15 #TKV0101
+
+SELECT f.title AS `Movies similiar to Pulp Beverly`
+FROM category AS c
+JOIN film_category AS fc
+USING(category_id)
+JOIN film AS f
+USING(film_id)
+JOIN film_actor AS fa
+USING(film_id)
+JOIN actor AS a
+USING(actor_id)
+WHERE c.name = 'Horror' AND
+concat(a.first_name, ' ', a.last_name) = 'KIRSTEN PALTROW' | 'DUSTIN TAUTOU' | 'SUSAN DAVIS' | 'WARREN NOLTE' | 'GRETA KEITEL' | 'WHOOPI HURT' | 'ANGELA WITHERSPOON'
+GROUP BY f.title;
