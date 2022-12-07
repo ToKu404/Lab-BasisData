3a88f043f71e63df208005a006d1ae2b692a43d0
PutriAuliaNur <ptrslsbl1215@gmail.com>
2022-12-07 09:10:39 +0800

    Kumpul Remedial UAS (salah kumpul tadi)

--- H071211046/Remedial UAS/Remedial UAS.sql
+++ H071211046/Remedial UAS/Remedial UAS.sql
@@ -1,18 +1,13 @@
 #8 #AWM0101
 
-SELECT f.title AS `Movies similiar to Pulp Beverly`
+SELECT c.name AS Category, f.title as Title
 FROM category AS c
 JOIN film_category AS fc
 USING(category_id)
 JOIN film AS f
 USING(film_id)
-JOIN film_actor AS fa
-USING(film_id)
-JOIN actor AS a
-USING(actor_id)
-WHERE c.name = 'Horror' AND
-concat(a.first_name, ' ', a.last_name) = 'KIRSTEN PALTROW' | 'DUSTIN TAUTOU' | 'SUSAN DAVIS' | 'WARREN NOLTE' | 'GRETA KEITEL' | 'WHOOPI HURT' | 'ANGELA WITHERSPOON'
-GROUP BY f.title;
+WHERE LEFT(f.title, 1) IN ('A', 'I', 'U', 'E', 'O')
+ORDER BY RIGHT(f.title, 2) ASC;
 
 #13 #AZV0101
 
